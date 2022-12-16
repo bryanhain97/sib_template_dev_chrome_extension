e89b859eefae66fff0255d9c72a127fb8e3e951e
Bryan Hain <bryanhain97@gmail.com>
2022-12-16 23:30:39 +0100

    grid row n columns

--- src/components/Templates/Template.tsx
+++ src/components/Templates/Template.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, useRef } from 'react';
+import React, { useState, useCallback, ChangeEvent, useRef, MouseEvent } from 'react';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import YAML from 'yaml'
 export interface ITemplate {
@@ -8,6 +8,7 @@ export interface ITemplate {
 };
 type Input = {
     title: string,
+    columns: number,
     dist: string,
 }
 
@@ -17,9 +18,26 @@ const Template = ({
     clipboardText
 }: ITemplate
 ) => {
-    const inputRef = useRef<Input>({ title: '', dist: '' });
-    const handleChange = (e: ChangeEvent<HTMLInputElement>) => inputRef.current = { ...inputRef.current, [e.target.id]: e.target.value }
-    const yamlObject = YAML.stringify(clipboardText);
+    const objectRef = useRef(clipboardText);
+    const [input, setInput] = useState<Input>({ title: '', columns: 0, dist: '' })
+    const [applied, setApplied] = useState<Boolean>(false);
+
+    const handleChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+        setApplied(false);
+        setInput({ ...input, [e.target.id]: e.target.value } as Input)
+    }, [input]);
+
+    const createYAMLObject = (e: MouseEvent<HTMLButtonElement>) => {
+        objectRef.current = clipboardText;
+        [objectRef.current.title, objectRef.current.dist] = [input.title, input.dist];
+        for (let i = 0; i < input.columns; i++) {
+            // change padding-right for each col except last
+            // loop over classNames?
+        }
+        objectRef.current = YAML.stringify(objectRef.current)
+        setApplied(true);
+    };
+
     return (
         <div className="template">
             <img className="image" src={imgSrc} alt="copy to clipboard" />
@@ -33,9 +51,30 @@ const Template = ({
                     <label className="label" htmlFor="title">dist</label>
                     <input type="text" onChange={handleChange} id="dist" />
                 </div>
-                <CopyToClipboard text={yamlObject}>
-                    <button className="btn">copy</button>
-                </CopyToClipboard>
+                <div className="field">
+                    <label className="label" htmlFor="columns">columns</label>
+                    <input type="text" onChange={handleChange} id="columns" />
+                </div>
+                {input.columns > 1 &&
+                    <div className="padding-fields">
+                        {
+                            new Array(input.columns - 1).fill(0).map((x, i) => {
+                                return (
+                                    <div className="padding-field" key={i}>
+                                        <label className="label" htmlFor={`padding-${i + 1}`}>pr col-{i + 1}</label>
+                                        <input className="input" id={`padding-${i + 1}`} />
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
+                }
+                <div className="buttons">
+                    <button className="btn" onClick={createYAMLObject}>create YAML</button>
+                    <CopyToClipboard text={objectRef.current}>
+                        <button className={applied ? 'btn btn_copy' : 'btn btn-copy disabled'}>copy</button>
+                    </CopyToClipboard>
+                </div>
             </div>
         </div>
     )
--- src/components/Templates/Templates.tsx
+++ src/components/Templates/Templates.tsx
@@ -4,6 +4,7 @@ import './Templates.sass';
 import clipboardText from '../../data.json';
 
 const Templates = () => {
+    // create template context and have createYAML fn here
     return (
         <div className="templates">
             {clipboardText.templates.map(({ imgSrc, description, clipboardText }: ITemplate, i) => {
